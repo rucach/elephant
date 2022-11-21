826e8c1e6cb48903d2bae620c384c494617db6ec
Rucach <98107806+rucach@users.noreply.github.com>
2022-11-21 19:56:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,135 @@
+
+song = "";
+objects = [];
+status = "";
+
+function preload()
+{
+	/*start code here*/
+  song = loadSound("Alarm.mp3");
+}
+
+function setup() {
+  /*start code here*/
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380, 380);
+  video.hide();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function gotResult(error, results) {
+  /*start code here*/
+  if(error)
+  {
+    console.log(error);
+  }else
+  {
+    console.log(results);
+    objects = results;
+  }
+}
+
+
+function draw() {
+  image(video, 0, 0, 380, 380);
+      if(status != "")
+      {
+        r =  random(255);
+        g =  random(255);
+        b =  random(255);      
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+ 
+          fill(r,g,b);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(r,g,b);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+         
+          if(objects[i].label == "elephant")
+          {
+            /*start code here*/
+            document.getElementById("number_of_objects").innerHTML = "elephand found";
+            console.log("play");
+            song.play();
+          }
+          else
+          {
+            document.getElementById("number_of_objects").innerHTML = "elephant not found";
+            console.log("stop");
+            song.stop();
+            /*start code here*/
+          }
+         }
+
+        if(objects.length == 0)
+        {
+          /*start code here*/
+          document.getElementById("number_of_objects").innerHTML = "elephant not found";
+          console.log("stop");
+          song.stop();
+        }
+
+        // pig
+
+        if(objects[i].label == "pig")
+          {
+            /*start code here*/
+            document.getElementById("number_of_objects").innerHTML = "pig found";
+            console.log("play");
+            song.play();
+          }
+          else
+          {
+            document.getElementById("number_of_objects").innerHTML = "pig not found";
+            console.log("stop");
+            song.stop();
+            /*start code here*/
+          }
+         }
+
+        if(objects.length == 0)
+        {
+          /*start code here*/
+          document.getElementById("number_of_objects").innerHTML = "pig not found";
+          console.log("stop");
+          song.stop();
+        }
+
+        // goat
+
+        if(objects[i].label == "goat")
+          {
+            /*start code here*/
+            document.getElementById("number_of_objects").innerHTML = "goat found";
+            console.log("play");
+            song.play();
+          }
+          else
+          {
+            document.getElementById("number_of_objects").innerHTML = "goat not found";
+            console.log("stop");
+            song.stop();
+            /*start code here*/
+          }
+         }
+
+        if(objects.length == 0)
+        {
+          /*start code here*/
+          document.getElementById("number_of_objects").innerHTML = "goat not found";
+          console.log("stop");
+          song.stop();
+        }
+      }
+}
